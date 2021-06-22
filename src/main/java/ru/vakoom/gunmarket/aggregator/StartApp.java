bd6745f6514287cffd884711f4f0056750678bd0
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2021-06-22 22:58:38 +0300

    main: fist commit (errorcommondatalayer can't download)

--- src/main/java/ru/vakoom/gunmarket/aggregator/StartApp.java
+++ src/main/java/ru/vakoom/gunmarket/aggregator/StartApp.java
@@ -2,10 +2,19 @@ package ru.vakoom.gunmarket.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
-@SpringBootApplication
+@EntityScan("ru.vakoom.gunmarket.commondatalayer.model")
+@EnableJpaRepositories({
+        "ru.vakoom.gunmarket.commondatalayer.repo",
+        "ru.vakoom.querybuilder"
+})
+@SpringBootApplication(scanBasePackages = {
+        "ru.vakoom.gunmarket.aggregator",
+        "ru.vakoom.querybuilder"
+})
 public class StartApp {
 
     public static void main(String[] args) {
