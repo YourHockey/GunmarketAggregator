90c265673a7d17cddfbd77006819a1025d24a398
abrosimov94vladislav@gmail.com <abrosimov94vladislav@gmail.com>
2021-10-05 15:50:49 +0300

    develop: config fix

--- docker/init-gunmarketx.sql
+++ docker/init-gunmarketx.sql
@@ -1,1 +1,21 @@
 Select 1;
+
+INSERT INTO public.type (type_id, lower, medium, show_name, upper) VALUES (1, null, 'ГЛАДКОСТВОЛЬНОЕ_ОРУЖИЕ', 'Гладкоствольное оружие', 'ОРУЖИЕ');
+INSERT INTO public.type (type_id, lower, medium, show_name, upper) VALUES (2, null, 'НАРЕЗНОЕ_ОРУЖИЕ', 'Нарезное оружие', 'ОРУЖИЕ');
+
+INSERT INTO public.shop (shop_id, name, shop_info, website) VALUES (1, 'shop', 'shop', 'shop');
+
+INSERT INTO public.brand (brand_id, full_name, short_name) VALUES (1, 'Концерн Калашников', 'Концерн Калашников');
+INSERT INTO public.brand (brand_id, full_name, short_name) VALUES (2, 'Молот Оружие', 'Молот');
+INSERT INTO public.brand (brand_id, full_name, short_name) VALUES (3, 'Молот Армз', 'Молот Армз');
+
+INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (4, null, null, null, null, 'Карабин TR9', 'https://kalashnikov.market/product-images/9956/_0.jpg', 1, 1);
+INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (5, null, null, null, null, 'Карабин TG2 Magnum', 'https://kalashnikov.market/product-images/16587/TG2;%20366%20Magnum;%20415%20%d0%bc%d0%bc_1.jpg', 1, 1);
+INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (6, null, null, null, null, 'Карабин TG2', 'https://kalashnikov.market/product-images/9947/_0.jpg', 1, 1);
+INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (7, null, null, null, null, 'Карабин TG2 исп 03', 'https://kalashnikov.market/product-images/16056/TG2%20%d0%b8%d1%81%d0%bf.03;%20366%20%d0%a2%d0%9a%d0%9c;%20341%20%d0%bc%d0%bc_1.jpg', 1, 1);
+INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (8, null, null, null, null, 'Карабин TR3', 'https://kalashnikov.market/product-images/14342/TR3_366_1.jpg', 1, 1);
+INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (9, null, null, null, null, 'Карабин TG3 01', 'https://kalashnikov.market/product-images/12535/G6363.jpg', 1, 1);
+INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (10, null, null, null, null, 'Карабин TG3 02', 'https://kalashnikov.market/product-images/9936/_0.jpg', 1, 1);
+INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (11, null, null, null, null, 'Карабин TG3', 'https://kalashnikov.market/product-images/9944/_0.jpg', 1, 1);
+
+Select 1;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,19 @@
 SELECT 1;
 
--- INSERT INTO public.type (lower, medium, show_name, upper) VALUES (null, 'ГЛАДКОСТВОЛЬНОЕ_ОРУЖИЕ', 'Гладкоствольное оружие', 'ОРУЖИЕ');
+-- INSERT INTO public.type (type_id, lower, medium, show_name, upper) VALUES (1, null, 'ГЛАДКОСТВОЛЬНОЕ_ОРУЖИЕ', 'Гладкоствольное оружие', 'ОРУЖИЕ');
+-- INSERT INTO public.type (type_id, lower, medium, show_name, upper) VALUES (2, null, 'НАРЕЗНОЕ_ОРУЖИЕ', 'Нарезное оружие', 'ОРУЖИЕ');
 --
--- INSERT INTO public.brand (full_name, short_name) VALUES ('Концерн Калашникова', 'КК');
+-- INSERT INTO public.shop (shop_id, name, shop_info, website) VALUES (1, 'shop', 'shop', 'shop');
 --
--- INSERT INTO public.shop (name, shop_info, website) VALUES ('TestShop', 'TestInfo', 'TestShop.ru');
+-- INSERT INTO public.brand (brand_id, full_name, short_name) VALUES (1, 'Концерн Калашников', 'Концерн Калашников');
+-- INSERT INTO public.brand (brand_id, full_name, short_name) VALUES (2, 'Молот Оружие', 'Молот');
+-- INSERT INTO public.brand (brand_id, full_name, short_name) VALUES (3, 'Молот Армз', 'Молот Армз');
 --
--- INSERT INTO public.product (name, extended_name, brand_id, type_id, min_price, max_price) VALUES ('TestGun1','TestGunExt1',1,1,100,200);
--- INSERT INTO public.product (name, extended_name, brand_id, type_id, min_price, max_price) VALUES ('TestGun2','TestGunExt2',1,1,100,200);
---
--- INSERT INTO public.product_compatible_product (product_product_id,compatible_product_product_id) VALUES (1,2);
--- INSERT INTO public.product_compatible_product (product_product_id,compatible_product_product_id) VALUES (2,1);
---
--- INSERT INTO public.offer (offer_id, product_id,shop_id, price, link, in_stock) VALUES (1,1,1,100,'link',true);
--- INSERT INTO public.offer (offer_id, product_id,shop_id, price, link, in_stock) VALUES (2,2,1,150,'link',false);
+-- INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (4, null, null, null, null, 'Карабин TR9', 'https://kalashnikov.market/product-images/9956/_0.jpg', 1, 1);
+-- INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (5, null, null, null, null, 'Карабин TG2 Magnum', 'https://kalashnikov.market/product-images/16587/TG2;%20366%20Magnum;%20415%20%d0%bc%d0%bc_1.jpg', 1, 1);
+-- INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (6, null, null, null, null, 'Карабин TG2', 'https://kalashnikov.market/product-images/9947/_0.jpg', 1, 1);
+-- INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (7, null, null, null, null, 'Карабин TG2 исп 03', 'https://kalashnikov.market/product-images/16056/TG2%20%d0%b8%d1%81%d0%bf.03;%20366%20%d0%a2%d0%9a%d0%9c;%20341%20%d0%bc%d0%bc_1.jpg', 1, 1);
+-- INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (8, null, null, null, null, 'Карабин TR3', 'https://kalashnikov.market/product-images/14342/TR3_366_1.jpg', 1, 1);
+-- INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (9, null, null, null, null, 'Карабин TG3 01', 'https://kalashnikov.market/product-images/12535/G6363.jpg', 1, 1);
+-- INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (10, null, null, null, null, 'Карабин TG3 02', 'https://kalashnikov.market/product-images/9936/_0.jpg', 1, 1);
+-- INSERT INTO public.product (product_id, extended_name, image_url, max_price, min_price, name, src_image_url, brand_id, type_id) VALUES (11, null, null, null, null, 'Карабин TG3', 'https://kalashnikov.market/product-images/9944/_0.jpg', 1, 1);
